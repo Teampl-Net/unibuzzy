b000ebd36c791a7da759f761d117432dac9f1fdb
JeongJun Jo <rgfdds98.dev@gmail.com>
2023-10-27 18:51:37 +0900

    Feat: set file server url condition

--- src/components/common/CommonAddContentsForm.vue
+++ src/components/common/CommonAddContentsForm.vue
@@ -391,7 +391,7 @@ export default defineComponent({
           form.append('files[0]', newFile)
           await axios
             // 파일서버 fileServer fileserver FileServer Fileserver
-            .post('https://unibuzzy.com/file/tp.uploadFile', form, {
+            .post(props.pOptions.fileServerURL, form, {
               headers: {
                 'Content-Type': 'multipart/form-data; charset: UTF-8;'
               }
