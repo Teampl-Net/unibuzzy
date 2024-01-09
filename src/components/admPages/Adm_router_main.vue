22a9c9a539725dbef055aacad4a03414eb4d91e0
SoyeonKim <145084748+borimomSoyeon@users.noreply.github.com>
2024-01-09 17:39:12 +0900

    hybric admin page processing

---
+++ src/components/admPages/Adm_router_main.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="routerRef" class="w-100P h-100P mainBackgroundColor listRefresh" style="overflow:hidden"  >
+  <commonHeader />
+  <router-view style="padding:60px 20px 0;"></router-view>
+  <commonFooter v-if="footer" />
+  </div>
+</template>
+
+<script>
+import commonHeader from '@/components/admPages/commonCompo/Adm_commonHeader.vue'
+import commonFooter from '@/components/admPages/commonCompo/Adm_commonFooter.vue'
+export default {
+  components: {
+    commonHeader,
+    commonFooter
+  },
+  data () {
+    return {}
+  },
+  created () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/admPages/commonCompo/Adm_commonFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="commonFooter"> hi? </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  created () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
