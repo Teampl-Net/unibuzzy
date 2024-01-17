37d1f3898e7c83893e4e07c98fd034b9b08cf8d2
SoyeonKim <145084748+borimomSoyeon@users.noreply.github.com>
2024-01-17 12:10:28 +0900

    edit css

--- src/pages/routerPages/TodoList.vue
+++ src/pages/routerPages/TodoList.vue
@@ -3060,7 +3060,8 @@ svg > path {
   width: 100%;
   height: 100%;
   position: fixed;
-  background: #00000026;
+  /* background: #00000026; */
+  background: rgba(0,0,0,0.4);
   z-index: 12;
   top: 0;
   left: 0;
